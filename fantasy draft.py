618b0a969607864d6f85d562580fa71b1955aa69
Joshua Ragusin <44104701+jragusin@users.noreply.github.com>
2020-12-27 15:56:03 -0600

    fixed issue with sorting

--- fantasy draft.py
+++ fantasy draft.py
@@ -1,12 +1,19 @@
 import random
 
-managers = ["Suchir", "Joshua", "Seth", "Victoria"]
-avg_pos = {"Suchir":0, "Joshua":0, "Seth":0, "Victoria":0}
-for i in range(1,101):
-    init_draft = sorted(managers, key=lambda k: random.random())
+def draft():
+    managers = ["Suchir", "Joshua", "Seth", "Victoria"]
+    avg_pos = {"Suchir":0, "Joshua":0, "Seth":0, "Victoria":0}
+    for i in range(1,101):
+        init_draft = sorted(managers, key=lambda k: random.random())
+        for name in managers:
+            avg_pos[name] += init_draft.index(name)
     for name in managers:
-        avg_pos[name] += init_draft.index(name)
+        avg_pos[name] /= 100
+    results = {k: v for k, v in sorted(avg_pos.items(), key=lambda item: item[1])}
+    return results
 
-for i in sorted(avg_pos.keys()):
-    print(i, end=" ")
-    
+def main():
+    print(draft())
+
+if __name__ == "__main__":
+    main()
